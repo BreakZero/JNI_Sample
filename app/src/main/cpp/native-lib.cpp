b61451bdd6193418bd0021488ba207470fa34118
= <=>
2023-03-30 23:18:32 +0800

    init commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,54 @@
+#include <jni.h>
+#include "hash-library/headers/sha256.h"
+#include "hash-library/headers/md5.h"
+#include <string>
+#include "iostream"
+#include "android/log.h"
+
+#define LOG_TAG "EASY"
+
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,     LOG_TAG, __VA_ARGS__)
+
+class Student {
+private:
+    char* name;
+
+public:
+    char* getName() {
+        char buff[1024];
+        memset(buff, 0, sizeof(buff));
+        strcpy(buff, "Hello, ");
+        strcat(buff, name);
+        return buff;
+    }
+    Student(char *name) {
+        LOGI("调用构造函数");
+        this->name = name;
+    };
+
+    ~Student() {
+        LOGI("调用析构函数");
+    };
+};
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_easy_jni_1sample_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    MD5 md5;
+    std::string hello = "Hello from C++";
+    jstring result = env->NewStringUTF(md5(hello).c_str());
+    return result;
+}
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_easy_jni_1sample_MainActivity_updateName(JNIEnv *env, jobject thiz) {
+    jclass mainCls = env->GetObjectClass(thiz);
+    // jfieldID GetFieldID(jclass clazz, const char* name, const char* sig)
+    jfieldID nameField = env->GetFieldID(mainCls, "name", "Ljava/lang/String;");
+    Student* student = new Student("Dougie");
+    // void SetObjectField(jobject obj, jfieldID fieldID, jobject value)
+    env->SetObjectField(thiz, nameField, env->NewStringUTF(student->getName()));
+    delete student;
+    return env->NewStringUTF("hello world");
+}
---
+++ app/src/main/java/com/easy/jni_sample/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.easy.jni_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Printer
+import com.easy.jni_sample.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var name: String = "Dougie"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val s: Printer
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnUpdate.setOnClickListener {
+            updateName().also {
+                Log.d("=====", it)
+            }
+            binding.sampleText.text = name
+        }
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'jni_sample' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    external fun updateName(): String
+
+    companion object {
+        // Used to load the 'jni_sample' library on application startup.
+        init {
+            System.loadLibrary("jni_sample")
+        }
+    }
+
+    private fun testAnr() {
+
+        val lock1 = Object()
+        val lock2 = Object()
+
+        //子线程持有锁1，想要竞争锁2
+        thread {
+            synchronized(lock1) {
+                Thread.sleep(100)
+                synchronized(lock2) {
+                    Log.d("ANR", "testAnr: getLock2")
+                }
+            }
+        }
+
+        //主线程持有锁2，想要竞争锁1
+        synchronized(lock2) {
+            Thread.sleep(100)
+
+            synchronized(lock1) {
+                Log.d("ANR", "testAnr: getLock1")
+            }
+        }
+    }
+}
